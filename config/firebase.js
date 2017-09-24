3dee4c84628829b107655e4408c1e3164a751419
mikeconstable <mike@muology.me>
2017-09-24 22:30:25 +0100

    first commit

---
+++ config/firebase.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.4.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBmSm-QmaPRBWbgWew9p-3w7plvR7omdYA",
+    authDomain: "podly-33cf0.firebaseapp.com",
+    databaseURL: "https://podly-33cf0.firebaseio.com",
+    projectId: "podly-33cf0",
+    storageBucket: "podly-33cf0.appspot.com",
+    messagingSenderId: "816026390286"
+  };
+  firebase.initializeApp(config);
+</script>
